f398ce5d1c927d54e8142f009fb89675c80d851f
Bill Baer <wbaer@iliiliopae.local>
2021-01-10 16:43:16 +0000

    Updated site

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-printf "\033[0;32mBuilding css\033[0m\n"
+printf "\033[0;32mCompiling Saas\033[0m\n"
 
 sass assets/css/style.scss assets/css/style.min.css
 
-printf "\033[0;32mBuilding site\033[0m\n"
+printf "\033[0;32mGenerating site\033[0m\n"
 
 hugo serve
