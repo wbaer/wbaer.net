805af6c5ac8c2664918b00ec2ba360e8532c4719
wbaer <wbaer@wbaer.net>
2021-01-17 09:33:25 -0800

    Updated site and build tasks

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ printf "\033[0;32mRemoving previous build\033[0m\n"
 
 rm -rf public
 
+printf "\033[0;32mCleaning css\033[0m\n"
+
+rm -f assets/css/style.css assets/css/style.css.map
+
 printf "\033[0;32mCompiling Saas\033[0m\n"
 
 sass assets/css/style.scss assets/css/style.css
